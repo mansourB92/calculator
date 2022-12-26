216a0492d30d4b3c10dbd0e5a323582080b33d73
mansourB92 <mansour.abcz@gmail.com>
2022-12-26 12:22:14 +0330

    1

---
+++ calc.py
@@ -0,0 +1,23 @@
+# import math
+num1 = int(input("input frist number = \t"))
+op = input("\ninput operator : \t")
+num2 = int(input("\ninput secound number = \t"))
+out = ""
+if op == "+":
+	out = num1 + num2
+elif op == "-":
+	out = num1 - num2
+elif op == "*":
+	out = num1 * num2
+elif op == "/":
+	out = num1 / num2
+elif op == "**":
+	out = num1 ** num2
+elif op == "%":
+	out = num1 % num2
+else:
+	out="warning inputs"
+
+#if out != ""
+#	print(out)
+print("your result is : " + str(out))
